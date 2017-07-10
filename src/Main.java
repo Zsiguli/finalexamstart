2bb689d3665c1d38d96267d9a0816f00933cc3c1
zsiguli <markovichzsigmond@gmail.com>
2017-07-10 09:39:28 +0200

    chore(Joiner): add basic structure

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println(joiner(new ArrayList<>(), ""));
+  }
+
+  public static String joiner(List<String> stringsToJoin, String separator) {
+    String joinedString = new String();
+    return joinedString;
+  }
+}
