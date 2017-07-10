5e61a55c713392e571c364e3d82e16fbc2b40457
zsiguli <markovichzsigmond@gmail.com>
2017-07-10 09:48:29 +0200

    feat(joiner): version 1.0 ready

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(joiner(new ArrayList<>(), ""));
+    System.out.println(joiner(new ArrayList<>(Arrays.asList("first", "second", "third")), "b"));
   }
 
   public static String joiner(List<String> stringsToJoin, String separator) {
-    String joinedString = new String();
+    String joinedString = "original";
+    for (String str : stringsToJoin) {
+      if (joinedString.equals("original")) {
+        joinedString = str;
+      } else {
+        joinedString += separator + str;
+      }
+    }
     return joinedString;
   }
 }
